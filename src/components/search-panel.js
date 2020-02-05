a98b76a8d1dd3e9c9f7f7289ee37cee160cb619c
TatsianaH <ourflsun@gmail.com>
2020-02-05 00:59:34 -0500

    add components

---
+++ src/components/search-panel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPanel = () => {
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return <input
+        style = {searchStyle}
+        placeholder="search" />;
+};
+export default SearchPanel;
